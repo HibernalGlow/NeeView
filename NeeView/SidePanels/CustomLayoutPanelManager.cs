05d760b22c4417342ad5f2462591dbf02f2d1a4b
HibernalGlow <3090253564@qq.com>
2025-11-07 03:36:41 +0800

    fix: 更新子项目提交标识为脏状态

--- NeeView/SidePanels/CustomLayoutPanelManager.cs
+++ NeeView/SidePanels/CustomLayoutPanelManager.cs
@@ -82,6 +82,7 @@ namespace NeeView
                 nameof(ImageEffectPanel),
                 nameof(BookmarkPanel),
                 nameof(PlaylistPanel),
+                nameof(SuperResolutionPanel),
             };
 
             var panelLeftKeys = new[] { nameof(FolderPanel), nameof(PageListPanel), nameof(HistoryPanel) };
